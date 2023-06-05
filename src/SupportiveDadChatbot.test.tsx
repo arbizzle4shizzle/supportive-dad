9358e382854bacf450fa5518a4b684d358761a62
Arber Muharemi <arbermuharemi@gmail.com>
2023-06-05 18:51:05 -0400

    adding styling to website; crafted a bit of a story :)

--- src/SupportiveDadChatbot.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import SupportiveDadChatbot from './SupportiveDadChatbot';
-
-test('renders learn react link', () => {
-  render(<SupportiveDadChatbot />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- src/SupportiveDadChatbot.tsx
+++ src/SupportiveDadChatbot.tsx
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from 'openai';
+import './SupportiveDadChatbot.css';
 
-// Initialize the OpenAI API
-const openaiApiKey = process.env.REACT_APP_OPENAI_API_KEY
-const configuration = new Configuration({apiKey: openaiApiKey})
+const openaiApiKey = process.env.REACT_APP_OPENAI_API_KEY;
+const configuration = new Configuration({ apiKey: openaiApiKey });
 const openai = new OpenAIApi(configuration);
 
 const SupportiveDadChatbot: React.FC = () => {
-  const [messages, setMessages] = useState<ChatCompletionRequestMessage[]>([]);
+  const [stage, setStage] = useState(1);
+  const [response, setResponse] = useState('');
   const [userInput, setUserInput] = useState('');
+  const [messageSent, setMessageSent] = useState(false);
   const [imageURL, setImageURL] = useState<string | undefined>('');
 
   const trainingConversation: ChatCompletionRequestMessage[] = [
-    // Training conversation goes here
     { role: 'system', content: "You are a supportive dad." },
     { role: 'user', content: "I feel really down today." },
     { role: 'assistant', content: "I'm here for you. What's been bothering you?" },
@@ -22,6 +23,95 @@ const SupportiveDadChatbot: React.FC = () => {
     { role: 'assistant', content: "Don't lose hope. Job hunting can be challenging, but persistence is key. Keep refining your resume, expanding your network, and exploring different avenues for opportunities." },
   ];
 
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (stage === 1) {
+        setStage(2);
+      } else if (stage === 2) {
+        setStage(3);
+      } else if (stage === 3 && messageSent) {
+        setStage(4);
+      } else if (stage === 4 && response) {
+        setStage(5);
+      }
+    }, 4500);
+
+    return () => clearTimeout(timer);
+  }, [stage, messageSent, response]);
+
+  const handleReset = () => {
+    setStage(1);
+    setUserInput('');
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      event.preventDefault(); // Prevent the form submission (if any)
+      sendMessage();
+    }
+  }
+
+  const renderStage1 = () => (
+    <div className="stage-container">
+      <h1 className="fade-in-intro">Hey kid, heard there's something on your mind...</h1>
+    </div>
+  );
+
+  const renderStage2 = () => (
+    <div className="stage-container">
+      <h1 className="fade-in-intro">Don't worry, everything will be okay.</h1>
+      <h1 className="fade-in-intro">I'm here for you.</h1>
+    </div>
+  );
+
+  const renderStage3 = () => (
+    <div className="stage-container">
+    <h1 className={!messageSent ? 'fade-in' : 'fade-out'}>
+      Tell me, what's going on?
+    </h1>
+    <input
+      className={`input-box ${messageSent ? 'fade-out' : 'fade-in'}`}
+      type="text"
+      value={userInput}
+      onChange={handleUserInput}
+      placeholder="Enter your text..."
+      onKeyDown={handleKeyDown}
+    />
+    <button className={`send-button ${messageSent ? 'fade-out disabled' : 'fade-in'}`} onClick={sendMessage} disabled={messageSent} style={{ width: '100px' }}>
+      {messageSent ? 'Sending...' : 'Send'}
+    </button>
+    </div>
+  );
+
+  const renderStage4 = () => (
+    <div className="stage-container">
+      <h1 className={`${!response ? "fade-in" : "fade-out" }`}>Ah, I see...</h1>
+    </div>
+  );
+
+  const renderStage5 = () => {
+    // const phrases = ['Phrase 1', 'Phrase 2', 'Phrase 3'];
+    // const randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
+    const imagePhrase = "And here's a little picture you can hold on to to remind you of our conversation:"
+
+    return (
+      <div className="stage-container">
+        <h1 className="fade-in">{response}</h1>
+        {imageURL && (
+          <>
+            <h2 className="fade-in">{imagePhrase}</h2>
+            <img className="fade-in" src={imageURL} alt="A visual representation of your supportive dad's response" />
+          </>
+          )
+        }
+        <button className="fade-in reset-button" onClick={handleReset}>
+          There's something else...
+        </button>
+      </div>
+    );
+  };
+
   const handleUserInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     setUserInput(event.target.value);
   };
@@ -30,18 +120,13 @@ const SupportiveDadChatbot: React.FC = () => {
     if (userInput.trim() === '') {
       return;
     }
-
-    setMessages((prevMessages) => [
-      ...prevMessages,
-      { role: 'user', content: userInput },
-    ]);
+    setMessageSent(true)
 
     try {
-      // Send the conversation to OpenAI ChatGPT API
       const response = await openai.createChatCompletion({
         model: 'gpt-3.5-turbo',
         messages: [
-          ...trainingConversation, // Include the training conversation here
+          ...trainingConversation,
           { role: 'user', content: userInput },
         ],
         max_tokens: 256,
@@ -50,15 +135,12 @@ const SupportiveDadChatbot: React.FC = () => {
         presence_penalty: 0,
       });
 
-      // Extract the assistant's reply
       const assistantReply = response?.data.choices?.[0]?.message?.content;
 
       if (assistantReply) {
-        // Update the conversation with the assistant's reply
-        setMessages((prevMessages) => [...prevMessages,{ role: 'assistant', content: assistantReply }]);
+        setResponse(assistantReply)
         setUserInput('');
 
-        // Generate DALL-E image
         const imageResponse = await openai.createImage({
           prompt: assistantReply,
           size: '256x256'
@@ -66,34 +148,31 @@ const SupportiveDadChatbot: React.FC = () => {
 
         const imageUrl = imageResponse?.data.data?.[0]?.url;
         console.log('Generated image URL:', imageUrl);
-        
-        // Set the generated image URL
+
         setImageURL(imageUrl);
       }
-
-
     } catch (error) {
       console.log('Error:', error);
     }
   };
 
   return (
-    <div>
-      <div>
-        {messages.map((message, index) => (
-          <div key={index}>
-            {message.role === 'user' && <strong>You: </strong>}
-            {message.role === 'assistant' && <strong>Supportive Dad: </strong>}
-            {message.content}
-          </div>
-        ))}
-      </div>
-      <div>
-        {imageURL && <img src={imageURL} alt="A visual representation of your supportive dad's response" />}
-      </div>
-        <input type="text" value={userInput} onChange={handleUserInput} placeholder="Type your message..."/>
-        <button onClick={sendMessage}>Send</button>
-    </div>
+  <div className="app-container">
+    {stage === 1 && renderStage1()}
+    {stage === 2 && renderStage2()}
+    {stage === 3 && renderStage3()}
+    {stage === 4 && renderStage4()}
+    {stage === 5 && renderStage5()}
+  </div>
+    // <div className="container">
+
+    //   {imageURL && (
+    //     <div className="fade-in">
+    //       <img src={imageURL} alt="A visual representation of your supportive dad's response" />
+    //       {/* <button onClick={() => window.open(imageURL, '_blank')}>Save Image</button> */}
+    //     </div>
+    //   )}
+    // </div>
   );
 };
 
